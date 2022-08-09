a89a00bb498d11dab23960b414222b99e2a50d05
AndroidPoet <poetdroid2@gmail.com>
2022-08-09 10:17:25 +0530

    first commit

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_androidpoet_currencyconverter_CurrencyApp.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.androidpoet.currencyconverter.CurrencyApp"
-)
-public final class _com_androidpoet_currencyconverter_CurrencyApp {
-}
